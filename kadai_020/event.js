c5a183d4bf587fa0720a41f20a80cd31ce5fc958
Ringogo55 <akiko.yeah@gmail.com>
2024-09-03 22:24:04 +0900

    kadai_020を修正しました

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,9 +1,11 @@
 //btnというid要素を持つHTML要素を取得し、定数に代入する
-const btn = document.getElementById('btn');
+const btnElement = document.getElementById('btn');
+
+//textというid要素を持つHTML要素を取得する
+const textElement = document.getElementById('text');
 
 //HTML要素がクリックされた時にイベント実行する
-btn.addEventListener('click', () => {
-  
-  //textというid要素を持つHTML要素に「クリックしました」と表示する
-  text.textContent = 'ボタンをクリックしました'
+btnElement.addEventListener('click', () => {
+
+  textElement.textContent = 'ボタンをクリックしました'
 })
