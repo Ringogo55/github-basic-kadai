964fb323965f3d69e14e6c2584d72aaffe0dc2c6
Ringogo55 <akiko.yeah@gmail.com>
2024-09-05 06:42:45 +0900

    kadai_021をアップロードしました

---
+++ kadai_021/event.js
@@ -0,0 +1,12 @@
+//btnというid要素を持つHTML要素を取得し、定数に代入する
+const btnElement = document.getElementById('btn');
+
+//textというid要素を持つHTML要素を取得する
+const textElement = document.getElementById('text');
+
+//HTML要素がクリックされた時にイベント実行する
+btnElement.addEventListener('click', () => {
+setTimeout( () => {
+  textElement.textContent = 'ボタンをクリックしました'
+  }, 2000);
+});
